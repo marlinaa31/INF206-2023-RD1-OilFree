710c451e3f502d19257fb28cbf54b1d09ebb98ab
Ardiansyah <101657545+ansyahh@users.noreply.github.com>
2023-04-14 10:38:31 +0700

    Add files via upload

---
+++ resources/views/informasi.blade.php
@@ -0,0 +1,127 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Informasi</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+</head>
+
+<body class="bg-warning">
+    <nav class="navbar bg-body-tertiary">
+        <div class="container-fluid">
+
+            <button type="button" class="btn btn-Kembali">Kembali</button>
+            <a class="navbar-brand" href="#">Informasi</a>
+            <p align="center">
+            </p>
+        </div>
+    </nav>
+    <!-- <p class="align-items-center"></p> -->
+    <div class="d-flex justify-content-center mt-2 mb-2">
+        <div class="card mb-3" style=" max-width: 1200px;">
+            <div class="row g-0">
+                <div class="col-md-6">
+                    <img src="https://images.unsplash.com/photo-1600857544200-b2f666a9a2ec?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" class="img-fluid rounded-start" alt="...">
+                </div>
+                <div class="col-md-6">
+                    <div class="card-body">
+                        <h5 class="card-title">Sabun</h5>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+                            additional content. This content is a little bit longer.</p>
+                        <p class="card-text"><small class="text-body-secondary"></small></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- card 2 -->
+    <div class="d-flex justify-content-center mb-2">
+        <div class="card mb-3" style=" max-width: 1200px;">
+            <div class="row g-0">
+
+                <div class="col-md-6">
+                    <div class="card-body">
+                        <h5 class="card-title">Pupuk</h5>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+                            additional content. This content is a little bit longer.</p>
+                        <p class="card-text"><small class="text-body-secondary"></small></p>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <!-- <img src="../img/sabun.jpg" class="img-fluid rounded-start" style="width: 600px; height: 100%; object-fit: fill;" alt="..."> -->
+
+                    <img src="https://images.unsplash.com/photo-1609784969374-b53a14d91338?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" class="img-fluid rounded-start " alt="...">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="d-flex justify-content-center mb-2">
+        <div class="card mb-3" style=" max-width: 1200px;">
+            <div class="row g-0">
+                <div class="col-md-6">
+                    <img src="https://images.unsplash.com/photo-1630487378070-ec90568485b3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1098&q=80" class="img-fluid rounded-start" alt="...">
+                </div>
+                <div class="col-md-6">
+                    <div class="card-body">
+                        <h5 class="card-title">Pupuk</h5>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+                            additional content. This content is a little bit longer.</p>
+                        <p class="card-text"><small class="text-body-secondary"></small></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- <div class="card mb-3 d-flex align-items-center justify-content-center" style="max-width: 950px;">
+        <div class="row g-0">
+            <div class="col-md-4"><img src="../img/sabun.jpg" class="img-fluid rounded-start" alt="img/sabun.jpg">
+            </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title">Sabun</h5>
+                    <p class="card-text">
+                    <div>Bahan-bahan :</div>
+                    <div>500 gram minyak jelantah</div>
+                    <div>171 gram air mineral</div>
+                    <div>82 gram NaOH (soda api)</div>
+                    <div> ½ Bahan :</div>
+                    <div> 85 gram air mineral</div>
+                    <div>41 gram NaOH</div>
+                    <div>250 gram</div>
+                    <div> Perlengkapan :</div>
+                    <div>Spatula kayu, pengaduk atau mixer</div>
+                    <div>Timbangan digital</div>
+                    <div>Sarung tangan plastik</div>
+                    <div>Cetakan</div>
+                    <div>Cara Pembuatan :
+                        Campurkan NaOH ke dalam air mineral. Air tersebut akan panas, jadi biarkan dingin sesuai suhu
+                        ruang,
+                        tidak perlu ditiup atau diangin-anginkan. Ini kurang lebih 7 jam. Ketika NaOH dituang di air
+                        mineral, diaduk menggunakan bahan dari : kayu, plastik atau stainless steel ya. Kenapa? Karena
+                        soda
+                        api ini memiliki sifat korosif.
+                        Di wadah terpisah, tuangkan minyak jelantah, lalu masukkan air campuran NaOH.
+                        Aduk menggunakan spatula (kurang lebih 6 menit), dengan mixer (2 menit) sampai kental. Jika
+                        menggunakan mixer, pastikan gagang adukannya itu tidak digunakan untuk membuat kue, ya.
+                        Tuang ke dalam cetakan.
+                        Jika ingin memotong ke berbagai ukuran, tunggu 2-3 jam saat sudah mengeras.
+                        Sabun minyak jelantah ini tidak bisa langsung dipakai, ya. Harus didiamkan selama 14 hari agar
+                        prosesnya selesai atau set bener.</p>
+                        <p class="card-text"><small class="text-body-secondary"></small></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> -->
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
+        crossorigin="anonymous"></script>
+</body>
+
+
+</html>
