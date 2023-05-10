59d8759bd93ef48961405ed1419bf2412cd8185a
Ardiansyah <101657545+ansyahh@users.noreply.github.com>
2023-05-10 21:08:52 +0700

    Update masuk_pesanan.blade.php

--- resources/views/masuk_pesanan.blade.php
+++ resources/views/masuk_pesanan.blade.php
@@ -1,32 +1,68 @@
 @extends('layouts.layoutdeliveryman')
 
 @section('content')
+  <head>
+	    <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="{{ url('assets1/css/bootstrap.min.css')}}">
+	    <!----css3---->
+        <link rel="stylesheet" href="{{ url('assets1/css/custom.css')}}">
+		<!-- SLIDER REVOLUTION 4.x CSS SETTINGS -->
+	
+	    <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet">
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Delivery</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-</head>
-<body class="bg-warning">
-<nav class="navbar bg-body-tertiary">
-    <div class="container-fluid">
-        <a href="{{ route('dashboard_deliveryman') }}" class="btn btn-Kembali">Kembali</a>
-        <a class="navbar-brand" href="#">Pesanan</a>
-        <p align="center">
-        </p>
-    </div>
-</nav>
+	    <!--google material icon-->
+        <link href="https://fonts.googleapis.com/css2?family=Material+Icons"
+        rel="stylesheet">
+  </head>
+  <body>
+        <!-- Page Content  -->
+        <div id="content">
+		
+		<div class="top-navbar">
+            <nav class="navbar navbar-expand-lg">
+                <div class="container-fluid">
 
-<div class="wrapper">
-  
-@section('content')
-  <!-- Content -->
-<div class="container">
+                    <button type="button" id="sidebarCollapse" class="d-xl-block d-lg-block d-md-mone d-none">
+                        <span class="material-icons">arrow_back_ios</span>
+                    </button>
+					
+					<a class="navbar-brand" href="#"> Pesanan </a>
+					
+                    <button class="d-inline-block d-lg-none ml-auto more-button" type="button" data-toggle="collapse"
+					data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="material-icons">more_vert</span>
+                    </button>
 
-		<!-- Card untuk menampilkan pesanan -->
-		<div class="row row-cols-1 row-cols-md-3 g-4 mt-4">
+                    <div class="collapse navbar-collapse d-lg-block d-xl-block d-sm-none d-md-none d-none" id="navbarSupportedContent">
+                        <ul class="nav navbar-nav ml-auto">   
+                            <li class="dropdown nav-item active">
+                                <a href="#" class="nav-link" data-toggle="dropdown">
+                                   <span class="material-icons">person</span>
+                               </a>
+                                <ul class="dropdown-menu">
+                                    <li>
+                                        <a href="{{ route('profil_delivery') }}">
+                                            <span class="material-icons">person_outline</span>Profile
+                                          </a>
+                                    </li>
+                                    <li>
+                                        <a href="{{ route('welcome') }}">
+                                            <span class="material-icons">logout</span>Logout</a>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+	    </div>
+
+		<div class="main-content">
+			<div class="row">
+				<!-- Card untuk menampilkan pesanan -->
+			<div class=" col-lg-3 row-col-md-6 col-sm-6">
 			<!-- Card pertama -->
 			<div class="col">
 				<div class="card">
@@ -37,7 +73,9 @@
 					</div>
 				</div>
 			</div>
+		</div>
 			<!-- Card kedua -->
+			<div class=" col-lg-3 row-col-md-6 col-sm-6">
 			<div class="col">
 				<div class="card">
 					<div class="card-body">
@@ -47,7 +85,9 @@
 					</div>
 				</div>
 			</div>
+			</div>
 			<!-- Card ketiga -->
+			<div class=" col-lg-3 row-col-md-6 col-sm-6">
 			<div class="col">
 				<div class="card">
 					<div class="card-body">
@@ -57,7 +97,9 @@
 					</div>
 				</div>
 			</div>
+			</div>
 			<!-- Card keempat -->
+			<div class=" col-lg-3 row-col-md-6 col-sm-6">
 			<div class="col">
 				<div class="card">
 					<div class="card-body">
@@ -67,7 +109,9 @@
 					</div>
 				</div>
 			</div>
+			</div>
 			<!-- Card kelima -->
+			<div class=" col-lg-3 row-col-md-6 col-sm-6">
 			<div class="col">
 				<div class="card">
 					<div class="card-body">
@@ -77,7 +121,9 @@
 					</div>
 				</div>
 			</div>
+			</div>
             <!-- Card keenam -->
+			<div class=" col-lg-3 row-col-md-6 col-sm-6">
 			<div class="col">
 				<div class="card">
 					<div class="card-body">
@@ -88,12 +134,29 @@
 				</div>
 			</div>
 		</div>
+		</div>
 	</div>
 
-	<!-- Memanggil file JavaScript Bootstrap 5 -->
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
-        crossorigin="anonymous"></script>
-</body>
 
+  <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+	<script src="{{ url('assets1/js/jquery-3.3.1.slim.min.js') }}"></script>
+   <script src="{{ url('js/popper.min.js') }}"></script>
+   <script src="{{ url('js/bootstrap.min.js') }}"></script>
+   <script src="{{ url('js/jquery-3.3.1.min.js') }}"></script>
+  
+  
+  <script type="text/javascript">
+  $(document).ready(function () {
+            $('#sidebarCollapse').on('click', function () {
+                $('#sidebar').toggleClass('active');
+				$('#content').toggleClass('active');
+            });
+			
+			 $('.more-button,.body-overlay').on('click', function () {
+                $('#sidebar,.body-overlay').toggleClass('show-nav');
+            });
+			
+        });   
+</script>
 @endsection
