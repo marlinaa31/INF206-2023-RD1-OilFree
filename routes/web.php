16073362494604edc67c6227f28f0b02450263eb
Marlina <101453667+marlinaa31@users.noreply.github.com>
2023-05-11 00:54:28 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\deliveryman_controller;
 use Illuminate\Support\Facades\Auth;
 
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
