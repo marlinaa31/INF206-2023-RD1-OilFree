2dc6150e8e408883446fbc7b4e9577fa1ce75cd0
Marlina <101453667+marlinaa31@users.noreply.github.com>
2023-05-11 13:11:00 +0700

    Update deliveryman_controller.php

--- app/Http/Controllers/deliveryman_controller.php
+++ app/Http/Controllers/deliveryman_controller.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Pemesanan;
 use Illuminate\Http\Request;
 
 class deliveryman_controller extends Controller
@@ -9,7 +10,14 @@ class deliveryman_controller extends Controller
     //
     public function index()
     {
+        
         return view('dashboarddeliveryman');
+    }
+
+    public function takeOrder(){
+        $pemesanans = Pemesanan::all();
+        return view('masuk_pesanan', compact('pemesanans'));
+
     }
 
 }
