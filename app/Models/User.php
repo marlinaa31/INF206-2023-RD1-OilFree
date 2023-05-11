bb788ee46cd31255ff82ad065deed279da4aee54
Marlina <101453667+marlinaa31@users.noreply.github.com>
2023-05-11 13:14:37 +0700

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function pemesanan(){
+        return this->hasMany('\App\Pemesanan', 'user_id', 'id');
+    }
 }
