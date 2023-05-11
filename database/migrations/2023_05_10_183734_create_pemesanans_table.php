494efeb80069084fcb2765a5e6cc5f0e390a073b
Marlina <101453667+marlinaa31@users.noreply.github.com>
2023-05-11 13:18:39 +0700

    Add files via upload

---
+++ database/migrations/2023_05_10_183734_create_pemesanans_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pemesanans', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->string('nama');
+            $table->string('nohp');
+            $table->text('alamat');
+            $table->integer('berat_minyak');
+            $table->string('foto_minyak');
+            $table->date('tanggal');
+            $table->time('waktu');
+            $table->integer('status')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pemesanans');
+    }
+};
